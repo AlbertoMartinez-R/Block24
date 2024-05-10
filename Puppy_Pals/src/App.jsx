630aeed8a18e35df6e54040c54ad98650668845a
Alberto Martinez <avmrbeto98@gmail.com>
2024-05-09 19:26:16 -0700

    complete

--- Puppy_Pals/src/App.jsx
+++ Puppy_Pals/src/App.jsx
@@ -5,16 +5,62 @@ import './App.css'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
 
   return (
-    <div className='App'>
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
+
+    <div className='App'
+      style={{
+        width: '',
+      }}>
+      <h1 style={{
+        color: 'blue',
+        width: 'auto',
+        margin: '10px auto 200px'
+      }}
+      >
+        The best Puppies
+      </h1>
+
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}
+          style={{
+            color: 'whitesmoke',
+          }}>
+          {puppy.name}
+        </p>
+      ))}
+
+      {featPupId && featuredPup && (
+        <div className='Info'
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          <h2 style={{
+            marginTop: '50px',
+          }}
+          >{featuredPup.name}</h2>
+          <ul
+            style={{
+              listStyle: 'none',
+              alignContent: 'center',
+              color: 'crimson',
+            }}>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
